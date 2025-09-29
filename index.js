8d93506d51469b55dc1c1d5b9b9609fd55404702
JacobPoole84 <jacobpoole84@gmail.com>
2025-09-29 12:02:45 -0400

    progress

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ' modal__overlay--visible'
+    emailjs
+        .sendForm(
+            'service_tg7ucxv',
+            'template_4q1y68k',
+            event.target,
+            'gqTaTLEPy5k6HSxgY'
+        ).then(() => {
+            loading.classList.remove('modal__overlay--visible')
+            success.classList += ' modal__overlay--visible' 
+        }).catch(() => {
+            loading.classList.remove('modal__overlay--visible')
+            alert('The email service is temporarily unavailable. Please contact me directly at email@email.com')
+        })
+}
